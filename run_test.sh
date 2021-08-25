44b02499cfa7490283200fa0bd463efa5703a1d0
Joeyt1008 <87364343+Joeyt1008@users.noreply.github.com>
2021-08-25 14:26:02 +0530

    Update run_test.sh

--- run_test.sh
+++ run_test.sh
@@ -6,7 +6,7 @@ PROTOC_ZIP=protoc-3.17.3-linux-aarch_64.zip
 curl -OL https://github.com/protocolbuffers/protobuf/releases/download/v3.17.3/$PROTOC_ZIP
 unzip -o $PROTOC_ZIP -d /usr/local bin/protoc
 unzip -o $PROTOC_ZIP -d /usr/local include/*
-PROTOC=${PROTOC_ZIP:=protoc}
+PROTOC=${PROTOC:=protoc}
 echo $PROTOC
 echo $protoc
 PY_VER_MYPY_PROTOBUF=${PY_VER_MYPY_PROTOBUF:=3.8.11}
